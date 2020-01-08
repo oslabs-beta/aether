01f0af3472864c04659698c9bb99c9aec5a16c33
Vessy <v.shestorkina@gmail.com>
2020-01-08 16:21:57 -0500

    Refactored socket and updated function to get total memory of snapshot
    Co-authored by: Vessy <ve33y6@gmail.com>
    Co-authored by: Denis <belioglo@gmail.com>

--- aether_frontend/server/server.js
+++ aether_frontend/server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const port = 3000;
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
 server.listen(port);
 
 let heapData;
@@ -17,49 +20,47 @@ THE ON KEYWORD JUST DOES THAT -- LISTENS FOR A SPECIFIC EVENT
 ON REQUIRES 2 ARGUMENTS: A STRING CONTAINING THE TITLE OF THE EVENT EMITTED,
 ANMD A FUNCTION WITH WHICH THE DATA IS PASSED THROUGH.
 
-IN THE CASE OF OUR CONNECTION LISTENER, 
+IN THE CASE OF OUR CONNECTION LISTENER,
 WE USE SOCKET TO DEFINE THE DATA IN THE SECOND ARGUMENT.
 
   WHAT IS THE SOCKET? -- THE INDIVIDUAL CLIENT WHO IS CONNECTED
   WEBSOCKETS USE EVENT EMITTERS -- THE RECEIVING END IS LOOKING FOR A STRING
-  IF BOTH/HOWEVER MANY PARTIES HAVE ACCESS TO THE SAME EVENT LISTENER 
-  THEY CAN LISTEN FOR AND BROADCAST MESSAGES 
+  IF BOTH/HOWEVER MANY PARTIES HAVE ACCESS TO THE SAME EVENT LISTENER
+  THEY CAN LISTEN FOR AND BROADCAST MESSAGES
 
 */
 
 
-
-// AETHER'S WEBSOCKET SERVER ASSERTS THAT IT HAS A WEBSOCKET CONNECTION 
+// AETHER'S WEBSOCKET SERVER ASSERTS THAT IT HAS A WEBSOCKET CONNECTION
 // ANYONE ELSE THAT ALSO HAS A WEBSOCKET CONNECTION CAN NOW JOIN ON THAT SPECIFIC PORT
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   // console.log('connected:', socket.client.id);
-  //REMEMBER THERE COULD BE MULTIPLE SOCKETS
+  // REMEMBER THERE COULD BE MULTIPLE SOCKETS
   // WE'RE LISTENING FOR A RESPONSE FROM TESTING SAMPLE APP
   // AETHER BEGINS LISTENS FOR A ONE PARTICULAR EVENT CALLED SERVEREVENT
   // IF DATA GETS EMITTED AT SERVEREVENT WE REASSIGN HEAP DATA CONTINOUSLY
-  socket.on('serverEvent', function(data) {
+  socket.on('serverEvent', (data) => {
     // console.log('new message from client:', data);
     heapData = data;
   });
   // SETINTERVAL CONTAINS TRIGGERS THE CLIENT-EVENT
   // THIS IS THE FIRST REAL EVENT THAT IS HAPPENING, OTHERWISE
   // IT'S JUST AN OPEN CONNECTION THAT DOES NOTHING
-  setInterval(function() { //TRIGGERS THE CLIENT- EVENT,
-
-    // EMIT IS BROADCASTING SAYING I HAVE A CLIENTEVENT!
-    // TESTING SAMPLE APP IS SAYING HEY! I AM CLIENT EVENT AND I HAVE SOMETHING TO SAY
-    socket.emit('clientEvent', 'update snapshot request');
-    // console.log('message sent to the clients');
-  }, 3000);
+  // setInterval(() => { // TRIGGERS THE CLIENT- EVENT,
+  // EMIT IS BROADCASTING SAYING I HAVE A CLIENTEVENT!
+  // TESTING SAMPLE APP IS SAYING HEY! I AM CLIENT EVENT AND I HAVE SOMETHING TO SAY
+  socket.emit('clientEvent', 'update snapshot request');
+  // console.log('message sent to the clients');
+  // }, 3000);
 });
 
 
 app.use(express.static('/assets/'));
 
 app.get('/getdata', (req, res) => {
-  console.log("AT /GETDATA ENDPOINT", heapData);
+  console.log('AT /GETDATA ENDPOINT', heapData);
   res.status(200).send(heapData);
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
--- testing-sample-app/server/server.js
+++ testing-sample-app/server/server.js
@@ -3,48 +3,55 @@ const fs = require('fs');
 const path = require('path');
 const parser = require('heapsnapshot-parser');
 const io = require('socket.io-client');
-// REQUIRE THE DEPENDENCY AND CONNEC AT THE SPECIFIED PORT, 
+// REQUIRE THE DEPENDENCY AND CONNEC AT THE SPECIFIED PORT,
 // IF THE CONNECTION IS LOST ATTEMPT TO RECONNECT
 const socket = io.connect('http://localhost:3000/', {
-  reconnection: true
+  reconnection: true,
 });
 
 function takeSnapShot(input) {
   /* THE ORIGINAL FUCNTION'S MIDDLEWARE AHS BEEN REFACTORED TO NOW TAKE IN AN INPUT THAT IS AN OBJECT
-  IF WE PASS IN INPUT AS AN OBJECT AND GIVE IT A PROPERTY 
+  IF WE PASS IN INPUT AS AN OBJECT AND GIVE IT A PROPERTY
   WE PASS IT BY REFERENCE AND HENCE IT EXISTS OUTSIDE OF THE FUNCTION
   OTHERWISE IT WOULD NOT PERSIST AND BE UNDEFINED
   */
 
   const filename = `../snapshot/${Date.now()}.heapsnapshot`;
-  console.log('Filename: ', filename);
-  
-  heapdump.writeSnapshot(path.resolve(__dirname, filename), function(err, filename) {
+  // console.log('Filename: ', filename);
+
+  heapdump.writeSnapshot(path.resolve(__dirname, filename), (err, filename) => {
     const snapshotFile = fs.readFileSync(filename, { encoding: 'utf-8' });
     const snapshot = parser.parse(snapshotFile);
-    const parsedSnapShot = [];
+    // console.log(snapshot);
+    let selfSizeTotal = 0;
+    const snapshotArray = Object.keys(snapshot.nodes);
 
-    for (let i = 0; i < 5; i++) {
+    // return selfSizeTotal;
+    // snapshot is a big object
+    // access each node.self_size
+    // we need to grab self-size
+    // add all of the self-sizes to a total
+    // return the total
+
+    for (let i = 0; i < snapshotArray.length; i += 1) {
       const node = snapshot.nodes[i];
-      parsedSnapShot.push({
-        Type: node.type,
-        Name: node.name,
-        ID: node.id,
-        self_size: node.self_size,
-        edge_count: node.edge_count
-      });
+      selfSizeTotal += node.self_size;
     }
-    console.log(parsedSnapShot);
+    console.log(selfSizeTotal);
+    input.input = JSON.stringify(selfSizeTotal);
     // GIVE THE INPUT OBJECT WE PASSED IN A PROPERTY CALLED INPUT
-    // THAT INPUT PROPERTY GETS JSON STRINGIFIED AS THE RESULT OF WHAT PARNSEDSNAPSHOT SPITS OUT
-    input.input = JSON.stringify(parsedSnapShot);
-
+    // THAT INPUT PROPERTY GETS JSON STRINGIFIED AS THE RESULT OF WHAT PARNSEDSNAPSHOT RETURNS
   });
+  // fs.unlink(filename, (err) => {
+  //   if (err) throw err;
+  //   console.log('succesfully deleted', filename);
+  // });
 }
+
 
 // ESTABLISHES AND CONNECT ON YOUR SOCKET CONNECTION
 // JUST BEFORE THE CONNECTION IS ESTABLISHED HERE, IO.ON HAS BEEN TRIGGERED ON AETHER'S SERVER -- 33
-socket.on('connect', function() {
+socket.on('connect', () => {
   /*
   WE CREATED AN EMPTY CONTAINER OBJECT THAT WILL HOLD OUR PARSEDSNAPSHOT DATA.
   IF WE SIMPLY USED A VARIABLE -- THEN THE VARIABLE WOULD NOT EXIST OUTSIDE THE FUNCTION'S EXECUTION CONTEXT
@@ -54,15 +61,16 @@ socket.on('connect', function() {
   const newData = {};
   takeSnapShot(newData);
   console.log('connected to localhost:3000');
-  //NEED TO KNOW WHAT PARTICULAR EVENT TO LISTEN TO 
+  // NEED TO KNOW WHAT PARTICULAR EVENT TO LISTEN TO
 
-  //TESTING SAMPLE GETS THE EVENT LISTENER CLIENTEVENT TRIGGERED FROM AETHER-FRONTEND
+  // TESTING SAMPLE GETS THE EVENT LISTENER CLIENTEVENT TRIGGERED FROM AETHER-FRONTEND
   // AND THIS EVENT LISTENER HAS A CALLBACK FUNCTION
-  socket.on('clientEvent', function(data) {
+  socket.on('clientEvent', (data) => {
+    // takeSnapShot(newData);
     console.log('message from the server:', data);
     // AN EVENT IS EMITTED TO ALL CONNECTED CLIENTS -- CLIENTS CAN IDENTIFY THE EVENT BY THE INPUT STRING
 
-    // ONCE WE'VE FOUND THE PARTICULAR EVENT WE WANTED TO CONNECT WE BROADCAST THAT EVENBT TO ANYONE ELSE 
+    // ONCE WE'VE FOUND THE PARTICULAR EVENT WE WANTED TO CONNECT WE BROADCAST THAT EVENBT TO ANYONE ELSE
     // USING OUR SOCKET CONNECTION AND LISTENING TO THE SAME EVENT
     socket.emit('serverEvent', newData.input);
   });
